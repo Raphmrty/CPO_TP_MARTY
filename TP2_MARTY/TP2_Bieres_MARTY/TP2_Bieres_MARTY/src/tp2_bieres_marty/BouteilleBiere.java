2e0ae8a84b0ceebaddb2fc7f2dc98150c5fff998
raphael marty <marty@ZenbookR>
2023-10-04 15:29:27 +0200

    TP2 - Exercice 1 Bieres Q11

---
+++ TP2_MARTY/TP2_Bieres_MARTY/TP2_Bieres_MARTY/src/tp2_bieres_marty/BouteilleBiere.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_marty;
+
+/**
+ *
+ * @author marty
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+    public void lireEtiquette() {
+    System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) Brasserie : " + brasserie ) ;}
+    
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+
+    
+}
---
+++ TP2_MARTY/TP2_Bieres_MARTY/TP2_Bieres_MARTY/src/tp2_bieres_marty/TP2_Bieres_MARTY.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_marty;
+
+/**
+ * groupe : TDC
+ * Nom : MARTY
+ * @author marty
+ * rôle du programme : TP1_stats
+ * Date : 03/10/23 
+ */
+public class TP2_Bieres_MARTY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls", 7.0, "Dubuission") ;
+        uneBiere.ouverte = false;
+        uneBiere.lireEtiquette();
+        BouteilleBiere uneSecondeBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe") ;
+        uneSecondeBiere.ouverte = false;
+        uneBiere.lireEtiquette();
+        BouteilleBiere uneTroisiemeBiere = new Bouteillle("")
+                
+        
+    }
+    
+}
