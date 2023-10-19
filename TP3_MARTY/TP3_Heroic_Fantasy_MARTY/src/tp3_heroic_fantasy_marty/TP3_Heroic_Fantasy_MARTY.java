c8a0382ae5f6e7586011c3fa6710fb6a6a57fb5b
raphael marty <marty@ZenbookR>
2023-10-19 22:58:49 +0200

    TP3 - ajouter des armes aux personnages fait

--- TP3_MARTY/TP3_Heroic_Fantasy_MARTY/src/tp3_heroic_fantasy_marty/TP3_Heroic_Fantasy_MARTY.java
+++ TP3_MARTY/TP3_Heroic_Fantasy_MARTY/src/tp3_heroic_fantasy_marty/TP3_Heroic_Fantasy_MARTY.java
@@ -45,37 +45,37 @@ public class TP3_Heroic_Fantasy_MARTY {
         Guerrier Conan = new Guerrier("Conan",78,false);
         Guerrier Lannister = new Guerrier("Lanister",45,true);
         
-        ArrayList<Personnages> perso = new ArrayList<Personnages>();
-        perso.add(Gandalf);
-        perso.add(Garcimore);
-        perso.add(Conan);
-        perso.add(Lannister);
-        
-        System.out.println(perso);
+        ArrayList<Personnages> persos = new ArrayList<Personnages>();
+        persos.add(Gandalf);
+        persos.add(Garcimore);
+        persos.add(Conan);
+        persos.add(Lannister);
+        //Afficher les persos et leurs caractéristiques
+        System.out.println(persos);
         
         Magicien Merlin = new Magicien("Merlin", 90, true);
         Guerrier Legolass = new Guerrier("Legolass", 85, false);
-        Epee Carte = new Epee("Carte", 40, 1);
-        Epee Epéeendiamant = new Epee("Epee en diamant", 80, 50);
+        Epee Zora = new Epee("Zora", 55, 75);
+        Epee Epéedelegende = new Epee("Epee de legende", 100, 85);
         Epee Epéeenor = new Epee("Epee en or", 60, 40);
         Baton Massue = new Baton("Massue", 30, 10);
-        Baton Battedebaseball = new Baton("Batte de baseball", 35, 5);
+        Baton Mojo = new Baton("Mojo", 64, 99);
         Baton BatonMagique = new Baton("Baton magique", 90, 50);
         
         // Création des 6 armes et des 2 personnages
         
-        Merlin.Ajouter_Arme(Carte);
+        Merlin.Ajouter_Arme(Mojo);
         Merlin.Ajouter_Arme(Massue);
         Merlin.Ajouter_Arme(BatonMagique);
         
-        Legolass.Ajouter_Arme(Epéeendiamant);
+        Legolass.Ajouter_Arme(Epéedelegende);
         Legolass.Ajouter_Arme(Epéeenor);
-        Legolass.Ajouter_Arme(Battedebaseball);
+        Legolass.Ajouter_Arme(Zora);
         
         // Ajout des armes aux personnages
         
-        Merlin.Equiper_Arme("Massue");
-        Legolass.Equiper_Arme("Epee en diamant");
+        Merlin.Equiper_Arme("Mojo");
+        Legolass.Equiper_Arme("Epee de legende");
         
         // Equipement des armes
         
@@ -83,10 +83,7 @@ public class TP3_Heroic_Fantasy_MARTY {
         System.out.println(Legolass);
         
         // Affichage des personnages
-       
-        
-        
-        
+         
         System.out.println("Il y a " + Magicien.nbMagiciens + " magiciens");
         System.out.println("Il y a " + Guerrier.nbGuerriers + " guerriers");
         System.out.println("Il y a " + Personnages.nbPersos + " personnages");
